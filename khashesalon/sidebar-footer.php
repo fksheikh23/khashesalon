ae2ccbeee39c32c17eae08dfedd2901d1d89b71e
fksheikh23 <fk.sheikh@mail.utoronto.ca>
2015-08-04 16:46:50 -0400

    Fixed the spelling error dynami to dynamic

--- khashesalon/sidebar-footer.php
+++ khashesalon/sidebar-footer.php
@@ -11,6 +11,6 @@
  
  <div id="supplementary"> 
 	<div id="footer-widgets" class="footer-widgets widget-area clear" role="complementary">
-		<?php dynami_sidebar( 'sidebar-2' ); //If widgets are added to the footer sidebard, this code will be displayed ?>
+		<?php dynamic_sidebar( 'sidebar-2' ); //If widgets are added to the footer sidebard, this code will be displayed ?>
 	</div>
 </div>
