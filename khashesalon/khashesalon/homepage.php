4421d9c37241b6c1136f6f261626d7cf59bc26cd
menalkhan <menalpoly@hotmail.com>
2015-08-05 02:16:24 -0400

    homepage php template was added for the homepage

--- khashesalon/khashesalon/homepage.php
+++ khashesalon/khashesalon/homepage.php
@@ -1,30 +1,41 @@
 <?php
 /**
- * Template Name: Homepage Template
+ * Template Name:Homepage Template
  *
  * @package WordPress
- * @subpackage Khashesalon
+ * @subpackage khashesalon
  */
  
  get_header(); ?>
-	<div id="primary" class="content-area">
+ 
+ <div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		
 		<div class="photos"> 
-		<img src='images/01.jpg' /> 
-		<img src="images/02.jpg" /> 
-		<img src="images/03.jpg" /> 
-		<img src="images/04.jpg" /> 
-		<img src="images/05.jpg" /> 
-		<img src="images/06.jpg" />
-		<img src="images/07.jpg" /> 
-		<img src="images/08.jpg" />
-		<img src="images/10.jpg" /> 
-		<img src="images/11.jpg" /> 
-		<img src="images/13.jpg" /> 
-		<img src="images/14.jpg" /> 
-		</div>
-		</div>
+		<img src="http://s1.postimg.org/3nivnnc3j/image.jpg" width="250" height="250"> 
+		<img src="http://s9.postimg.org/sjaqk2ar3/image.jpg" width="250" height="250"> 
+		<img src="http://s12.postimg.org/ha4w4s0gt/image.jpg" width="250" height="190"> 
+		<img src="http://s13.postimg.org/n0g9zt4pj/image.jpg" width="250" height="250"> 
+		<img src="http://s23.postimg.org/afmtxhpnv/image.jpg" width="250" height="250">
+		<img src="http://s21.postimg.org/531fkk06f/image.jpg" width="250" height="250">
 		</div>
 
+			<?php while ( have_posts() ) : the_post(); ?>
 
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
+
