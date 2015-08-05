0ed0dab7f3757783a3487a6adbc17c7728960cf2
menalkhan <menalpoly@hotmail.com>
2015-08-05 00:32:45 -0400

    added gallery images

--- khashesalon/khashesalon/homepage.php
+++ khashesalon/khashesalon/homepage.php
@@ -9,6 +9,21 @@
  get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		<div class="photos"> 
+		<img src='images/01.jpg' /> 
+		<img src="images/02.jpg" /> 
+		<img src="images/03.jpg" /> 
+		<img src="images/04.jpg" /> 
+		<img src="images/05.jpg" /> 
+		<img src="images/06.jpg" />
+		<img src="images/07.jpg" /> 
+		<img src="images/08.jpg" />
+		<img src="images/10.jpg" /> 
+		<img src="images/11.jpg" /> 
+		<img src="images/13.jpg" /> 
+		<img src="images/14.jpg" /> 
+		</div>
+		</div>
 		</div>
 
 
