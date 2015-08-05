c432eaf1654609c039f6add36b89ed106fe0b8c1
menalkhan <menalpoly@hotmail.com>
2015-08-05 00:23:36 -0400

    added a homepage template for the homepage

---
+++ khashesalon/khashesalon/homepage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Homepage Template
+ *
+ * @package WordPress
+ * @subpackage Khashesalon
+ */
+ 
+ get_header(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		</div>
+
+
+<?php get_footer(); ?>
