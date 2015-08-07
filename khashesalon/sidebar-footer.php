d392296078b3244cc2fca195a6800481e57bed06
fksheikh23 <fk.sheikh@mail.utoronto.ca>
2015-08-07 02:02:23 -0400

    Hiding the sidebar

--- khashesalon/sidebar-footer.php
+++ khashesalon/sidebar-footer.php
@@ -8,7 +8,7 @@
 		return;
  }
  ?>
- 
+
  <div id="supplementary"> 
 	<div id="footer-widgets" class="footer-widgets widget-area clear" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-2' ); //If widgets are added to the footer sidebard, this code will be displayed ?>
